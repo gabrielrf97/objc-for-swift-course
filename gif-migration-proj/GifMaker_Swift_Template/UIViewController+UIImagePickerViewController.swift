f8d2779cf864b660732c84b25fa8fac100848df2
gabrielrf97 <gabrielrodriguesfaria97@gmail.com>
2023-02-18 18:22:31 -0300

    Migrating gif projects to new folder

---
+++ gif-migration-proj/GifMaker_Swift_Template/UIViewController+UIImagePickerViewController.swift
@@ -0,0 +1,59 @@
+//
+//  UIViewController+UIImagePickerViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Gabriel Rodrigues on 18/02/23.
+//  Copyright © 2023 Gabrielle Miller-Messner. All rights reserved.
+//
+import MobileCoreServices
+import UIKit
+
+extension UIViewController {
+    
+    
+}
+
+extension UIViewController: UIImagePickerControllerDelegate {
+    
+    func launchCamera() {
+        let picker = createPickerViewController(from: .camera)
+        self.present(picker, animated: true)
+    }
+    
+    func openGalery() {
+        let picker = createPickerViewController(from: .photoLibrary)
+        self.present(picker, animated: true)
+    }
+    
+    func createPickerViewController(from sourceType: UIImagePickerControllerSourceType) -> UIImagePickerController {
+        
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = sourceType
+        picker.mediaTypes = [kUTTypeMovie as String]
+        picker.allowsEditing = false
+        picker.delegate = self
+        
+        return picker
+    }
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let mediaType = info[UIImagePickerControllerMediaType] as? String else {
+            return
+        }
+        
+        if mediaType == (kUTTypeMovie as String),
+        let videoURL = info[UIImagePickerControllerMediaURL] as? NSURL,
+        let videoPath = videoURL.absoluteString {
+            UISaveVideoAtPathToSavedPhotosAlbum(videoPath, nil, nil, nil)
+            dismiss(animated: true)
+        }
+    }
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+}
+
+extension UIViewController: UINavigationControllerDelegate {}
---
+++ gif-migration-proj/GifMaker_Swift_Template/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Gabriel Rodrigues on 18/02/23.
+//  Copyright © 2023 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    
+    @IBAction func launchVideoCamera(sender: AnyObject) {
+        launchCamera()
+    }
+}
