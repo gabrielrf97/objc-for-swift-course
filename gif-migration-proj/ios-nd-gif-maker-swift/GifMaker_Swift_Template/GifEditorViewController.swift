d95dd7c739a3f2cae7cc3cfafdd699b1d1f357d3
gabrielrf97 <gabrielrodriguesfaria97@gmail.com>
2023-02-24 13:01:49 -0300

    Adding UITextField and KeyboardObservation methods in GifEditorViewController

--- gif-migration-proj/ios-nd-gif-maker-swift/GifMaker_Swift_Template/GifEditorViewController.swift
+++ gif-migration-proj/ios-nd-gif-maker-swift/GifMaker_Swift_Template/GifEditorViewController.swift
@@ -16,10 +16,67 @@ class GifEditorViewController: UIViewController {
     
     var gif: Gif?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        captionTextField.delegate = self
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        setGifImage()
+        subscribeToKeyboardNotifications()
+    }
+    
+    private func setGifImage() {
         if let gif = gif {
             gifImageView.image = gif.image
         }
+    }
+}
+
+//MARK: UITextFieldDelegate methods
+extension GifEditorViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.placeholder = nil
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+//MARK: KeyboardObservation methods
+extension GifEditorViewController {
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        if (self.view.frame.origin.y >= 0) {
+            var rect = self.view.frame;
+            rect.origin.y -= getKeyboardHeight(notification)
+            self.view.frame = rect;
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        if (self.view.frame.origin.y < 0) {
+            var rect = self.view.frame;
+            rect.origin.y += getKeyboardHeight(notification)
+            self.view.frame = rect;
+        }
+    }
+    
+    private func getKeyboardHeight(_ notification: Notification) -> Double {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrameEnd = userInfo[UIKeyboardFrameEndUserInfoKey] as? CGRect else { return 0.0 }
+        return keyboardFrameEnd.size.height;
     }
 }
